2bb1964feddf5689aeb70c12838239d956a80ef7
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2020-08-31 00:54:11 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Main from "./containers/Main";
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div>
       <Main />
-      <AmplifySignOut />
     </div>
   );
 }
 
 
-export default withAuthenticator(App);
+export default (App);
