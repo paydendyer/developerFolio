2ec5eb091e20c8a171df7b6f5c321e151444b604
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2020-08-31 00:25:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Main from "./containers/Main";
 
-function App() {
-  return (
-    <div>
-      <Main />
-    </div>
-  );
-}
-
-export default App;
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
