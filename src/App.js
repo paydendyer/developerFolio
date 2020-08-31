1a1c2cd84f3c9473a240201285294dc7b0d9db93
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2020-08-31 00:35:35 -0500

    Revert "Update App.js"
    This reverts commit 2ec5eb091e20c8a171df7b6f5c321e151444b604.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./containers/Main";
 
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
